bdcc489e74640ae6728a8750dae1de52a3efc135
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2014-03-08 19:08:12 -0600

    Try another method...

--- Rakefile.rb
+++ Rakefile.rb
@@ -8,33 +8,7 @@ end
 
 task :deploy do
   Dir.chdir "_site"
-  system 'echo "Working directory: "'
-  system "pwd"
-  system "Files"
-  system "ls"
-  system "git init"
-  system 'git remote add origin https://github.com/antonio-cabreraglz/drunken-octo-sansa.git'
-  system 'git config credential.helper "store --file=.git/credentials"'
-  system 'echo "https://${GH_TOKEN}:@github.com" > .git/credentials'
-  system "git checkout -b build"
-  system "git add ."
-  system 'git commit -am "build"'
-  
-  system 'git status'
-=begin  
-  File.open('.git/credentials', 'w') do |f|
-    f.write("https://#{ENV['GH_TOKEN']}:@github.com")
-  end
-=end
-  system "git fetch origin"
-  system "git checkout master"
-  system 'git checkout build'
-  system 'git branch -m master old-master'
-  system 'git branch -m build master'
-=begin
-  system "git merge -s ours master"
-  system "git checkout master"
-  system "git merge build"
-=end  
-  system "git push origin master"
+  system "git status"
+  system 'git commit -am "Deploy"'
+  system 'git push' 
 end
