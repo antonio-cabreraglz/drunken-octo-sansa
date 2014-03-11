b6d47570ef46a9668305dc65b0f94f292193750b
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2014-03-10 21:32:41 -0600

    Test on Travis

--- Rakefile.rb
+++ Rakefile.rb
@@ -5,13 +5,11 @@ require 'w3c_validators'
 
 include W3CValidators
 
-task :default => [:build]
+task :default => [:install_juicer, :test, :minify_css, :minify_js, :build]
 
 task :install_juicer do
   sh "juicer install yui_compressor"
   sh "juicer install jslint"
-  JSHint.config_path = "config/jshint.yml"
-
 end
 
 task :build do
@@ -32,19 +30,23 @@ task :deploy do
   Dir.chdir "_site"
   system "git status"
   system "git add ."
-  system 'git commit -am "Deploy"'
+  system 'git commit -am "Deploy #{ENV["TRAVIS_BUILD_NUMBER"]}"'
   system 'git push' 
 end
 
 task :minify_css do
   Dir.glob("**/*.css").each do |f|
-    sh "juicer merge #{f} --force"
+    unless f.end_with? ".min.css" 
+      sh "juicer merge #{f} --force"
+    end
   end
 end
 
 
 task :minify_js do
-  Dir.glob("**/*.css").each do |f|
-    sh "juicer merge #{f} --force"
+  Dir.glob("**/*.js").each do |f|
+    unless f.end_with? '.min.js' 
+      sh "juicer merge #{f} --force"
+    end
   end
 end
