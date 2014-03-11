ad7972494fe3262340c6a50a4862860e2a4af921
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2014-03-10 20:24:30 -0600

    Update Rakefile

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,6 +1,17 @@
 require 'rubygems'
+require 'w3c_validators'
+require 'jshint/tasks'
 
-task :default => [:build]
+include W3CValidators
+
+task :default => [:install_juicer, :build]
+
+task :install_juicer do
+  sh "juicer install yui_compressor"
+  sh "juicer install jslint"
+  JSHint.config_path = "config/jshint.yml"
+
+end
 
 task :build do
   sh 'jekyll build'
@@ -13,3 +24,18 @@ task :deploy do
   system 'git commit -am "Deploy"'
   system 'git push' 
 end
+
+task :minify_css do
+  Dir.glob("**/*.css").each do |f|
+    sh "juicer merge #{f} --force"
+  end
+end
+
+
+task :minify_js do
+  Dir.glob("**/*.css").each do |f|
+    sh "juicer merge #{f} --force"
+  end
+end
+
+
